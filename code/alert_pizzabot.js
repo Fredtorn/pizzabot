25536505691e1e64b9cbad091c91567ac3f03530
Fredtorn <fredrika.tornkvist@chasacademy.se>
2023-11-13 20:21:54 +0100

    edit

--- code/alert_pizzabot.js
+++ code/alert_pizzabot.js
@@ -14,11 +14,11 @@ const orderQuantity = parseInt(prompt("How many of " + orderName + " do you want
 console.log(orderName)
 if (orderName.toUpperCase() === vegetarian.toUpperCase() || orderName.toUpperCase() === hawaiian.toUpperCase() || orderName.toUpperCase() === pepperoni.toUpperCase()) {
     if (orderQuantity <= 2) {
-        alert("Great, I'll get started on your " + orderName + "right away, it will cost you " + (orderQuantity * pizzaPrice) + "kr. The pizzas will take 10 minutes.")
+        alert("Great, I'll get started on your " + orderName + " right away, it will cost you " + (orderQuantity * pizzaPrice) + "kr. The pizzas will take 10 minutes.")
     } if (orderQuantity >= 3 && orderQuantity <= 5) {
-        alert("Great, I'll get started on your " + orderName + "right away, it will cost you " + (orderQuantity * pizzaPrice) + "kr. The pizzas will take 15 minutes.")
+        alert("Great, I'll get started on your " + orderName + " right away, it will cost you " + (orderQuantity * pizzaPrice) + "kr. The pizzas will take 15 minutes.")
     } if (orderQuantity >= 6) {
-        alert("Great, I'll get started on your " + orderName + "right away, it will cost you " + (orderQuantity * pizzaPrice) + "kr. The pizzas will take 20 minutes.")
+        alert("Great, I'll get started on your " + orderName + " right away, it will cost you " + (orderQuantity * pizzaPrice) + "kr. The pizzas will take 20 minutes.")
     }
 } else {
     alert("Sorry, we don't have that pizza on our menu.");
